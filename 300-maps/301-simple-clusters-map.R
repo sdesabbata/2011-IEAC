a288f1815e1fe0e4d1876e683716b1f0e4a12a62
sdesabbata <s.desabbata@le.ac.uk>
2022-04-17 10:17:26 +0100

    Code for simple preliminary maps

---
+++ 300-maps/301-simple-clusters-map.R
@@ -0,0 +1,73 @@
+# Simple cluster map
+# 
+# It uses data from ISTAT distributed under CC BY 3.0 IT
+# Source: https://www.istat.it/it/archivio/104317
+# Legal notice: https://www.istat.it/it/note-legali
+# Licence: https://creativecommons.org/licenses/by/3.0/it/deed.en
+#
+# Author: Stefano De Sabbata
+# Date: 16 April 2022
+
+library(tidyverse)
+library(sf)
+library(tmap)
+
+preliminary_clustering_map <-
+  read_sf("storage/ieac_k08-with-vars-v0_0_4/ieac_k08-with-vars-v0_0_4.shp") %>% 
+  # mutate(ieac_k08 = as.character(ieac_k08)) %>% 
+  mutate(
+    ieac_k08 = recode(
+      ieac_k08,
+        `1` = "C",
+        `2` = "F",
+        `3` = "G",
+        `4` = "D",
+        `5` = "B",
+        `6` = "E",
+        `7` = "A",
+        `8` = "G"
+      )
+  ) %>% 
+  tmap::tm_shape() +
+  # Define the choropleth aesthetic
+  tmap::tm_polygons(
+    "ieac_k08",
+    title = "Preliminary\nclusters\n(v0.0.4)",
+    palette = "Set1",
+    legend.show = TRUE,
+    border.alpha = 0,
+    colorNA = NULL,
+    showNA = FALSE
+  ) +
+  # Define the layout
+  tmap::tm_layout(
+    frame = FALSE,
+    legend.title.size=1,
+    legend.text.size = 0.5,
+    legend.position = c("right","top")
+  ) +
+  # Don't forget the appropriate attribution
+  tmap::tm_credits(
+    "by Stefano De Sabbata\nThis is a work in progress, no conclusions should be drawn from this draft.\nhttps://github.com/sdesabbata/2011-IEAC\nSource: data from ISTAT distributed under CC BY 3.0 IT (https://www.istat.it/it/archivio/104317)",
+    size = 0.3,
+    position = c("left", "bottom")
+  )
+  
+# Save image
+tmap_save(
+  preliminary_clustering_map, 
+  "storage/ieac_k08-with-vars-v0_0_4-300dpi.png", 
+  width = 210, height = 298, asp=0,
+  units = "mm", dpi = 300
+  )
+
+# Save image
+tmap_save(
+  preliminary_clustering_map, 
+  "storage/ieac_k08-with-vars-v0_0_4-2400dpi.png", 
+  width = 210, height = 298, asp=0,
+  units = "mm", dpi = 2400
+)
+
+# Clear
+rm(list = ls())
